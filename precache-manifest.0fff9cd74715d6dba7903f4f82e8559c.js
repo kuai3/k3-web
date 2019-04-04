c6cd4651f413cc65c46f1195b2df0a768228e165
kuai3 <k3@kuai3.io>
2019-04-04 23:44:37 +0800

    k3 build

---
+++ precache-manifest.0fff9cd74715d6dba7903f4f82e8559c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7039d7ae5d5638e6837a7bf967a35822",
+    "url": "/static/media/1.7039d7ae.png"
+  },
+  {
+    "revision": "126b9cbe25c8bd369008",
+    "url": "/static/css/main.18ddc077.chunk.css"
+  },
+  {
+    "revision": "679d5ec38d71530f840d78633848bd13",
+    "url": "/static/media/cn.679d5ec3.svg"
+  },
+  {
+    "revision": "0f2b7b358d32694c5ea7",
+    "url": "/static/js/1.0f2b7b35.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0257545694d1ee1b12a08f2bca7c131c",
+    "url": "/static/media/dicen.02575456.png"
+  },
+  {
+    "revision": "126b9cbe25c8bd369008",
+    "url": "/static/js/main.126b9cbe.chunk.js"
+  },
+  {
+    "revision": "8a4a43fd4ef7573775cd280ab5457112",
+    "url": "/static/media/9-cn.8a4a43fd.png"
+  },
+  {
+    "revision": "3ec6d986757f7acf47a4c6aa36367ba2",
+    "url": "/static/media/9-en.3ec6d986.jpg"
+  },
+  {
+    "revision": "c4cd6642c20f6473ef5b38fbf9c3aa11",
+    "url": "/static/media/en.c4cd6642.svg"
+  },
+  {
+    "revision": "0f2b7b358d32694c5ea7",
+    "url": "/static/css/1.178a6479.chunk.css"
+  },
+  {
+    "revision": "5f3e4734eb27583acb0c0face5eec48d",
+    "url": "/index.html"
+  }
+];
